895d252129b110161d045d96a7e3690eaccb9230
hoomili <hasadian@hotmail.com>
2023-04-26 16:47:25 -0700

    getting tweets from the data file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-
 $(document).ready(function() {
   const createTweetElement = function(tweet) {
     const $newTweet = $(`
@@ -55,19 +29,37 @@ $(document).ready(function() {
     `);
     return $newTweet;
   };
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $tweetElement = createTweetElement(tweet);
       $('#tweets-container').append($tweetElement);
 
     }
   };
-  renderTweets(data);
+
+  // sendig the form data to the server
   const $form = $('form');
   $form.submit(function(event) {
     event.preventDefault();
-    console.log(event);
+    const data = $(this).serialize();
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: data
+    }).then(console.log(data));
   });
+  
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    }).then(function(tweets) {
+      console.log('Success: ', tweets);
+      renderTweets(tweets);
+    });
+  };
+  loadTweets();
+
 });
 
 
